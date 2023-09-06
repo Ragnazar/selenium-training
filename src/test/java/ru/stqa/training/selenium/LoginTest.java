bb955dbe7dd21991fb106c613ad1e84c75826eb6
Ivan Avdeyev <97154352+Ragnazar@users.noreply.github.com>
2023-09-06 22:51:00 +0300

    Задание 3

--- src/test/java/ru/stqa/training/selenium/LoginTest.java
+++ src/test/java/ru/stqa/training/selenium/LoginTest.java
@@ -6,14 +6,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
+import java.util.concurrent.TimeUnit;
 
-public class MyFirstTest {
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+public class LoginTest {
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -23,21 +24,23 @@ public class MyFirstTest {
     }
 
     @BeforeEach
-    public void setup() {
+    public void setUp() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 5);
-    }
-
-    @Test
-    public void myFirstTest() {
-        driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        wait.until(titleContains("webdriver"));
     }
 
     @AfterEach
     public void teardown() {
         driver.quit();
+    }
+
+    @Test
+    public void adminUserShouldLogIn(){
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(presenceOfElementLocated(By.className("logotype")));
     }
 }
