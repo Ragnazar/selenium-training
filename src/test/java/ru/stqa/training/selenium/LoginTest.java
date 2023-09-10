8d692d17f0a3d56b35fea7a918bde0812bdcf08e
Ivan Avdeyev <97154352+Ragnazar@users.noreply.github.com>
2023-09-10 17:51:19 +0300

    firefox-nighty browser works with firefox webdriver

--- src/test/java/ru/stqa/training/selenium/LoginTest.java
+++ src/test/java/ru/stqa/training/selenium/LoginTest.java
@@ -7,11 +7,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
@@ -31,10 +29,7 @@ public class LoginTest {
     @BeforeEach
     public void setUp() {
         FirefoxBinary binary = new FirefoxBinary(new File("C:\\Program Files\\Firefox Nightly\\firefox.exe"));
-        FirefoxProfile profile = new FirefoxProfile();
-
-        //  System.setProperty("webdriver.firefox.bin", "\"C:\\Program Files\\Firefox Nightly\\firefox.exe\"");
-        driver = new FirefoxDriver();
+        driver = new FirefoxDriver(new FirefoxOptions().setBinary(binary));
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 5);
     }
