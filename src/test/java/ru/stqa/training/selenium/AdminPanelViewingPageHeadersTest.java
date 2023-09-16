dbec6755c53a56799b4124bb1885d4d1bf6e6420
Ivan Avdeyev <97154352+Ragnazar@users.noreply.github.com>
2023-09-16 23:17:53 +0300

    Задание 6. Сделайте сценарий, проходящий по всем разделам админки

--- src/test/java/ru/stqa/training/selenium/AdminPanelViewingPageHeadersTest.java
+++ src/test/java/ru/stqa/training/selenium/AdminPanelViewingPageHeadersTest.java
@@ -1,24 +1,18 @@
 package ru.stqa.training.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-
 public class AdminPanelViewingPageHeadersTest {
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @BeforeAll
     public static void setupAll() {
@@ -30,7 +24,11 @@ public class AdminPanelViewingPageHeadersTest {
 
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, 5);
+
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
 
 
     }
@@ -42,26 +40,27 @@ public class AdminPanelViewingPageHeadersTest {
 
     @Test
     public void adminUserShouldClickOnMenuItemAndFindHeader() {
-        driver.get("http://localhost/litecart/admin/");
-        driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.name("login")).click();
+
 
         List<WebElement> list = driver.findElements(By.cssSelector("li#app-"));
 
-//        List<WebElement> docs = driver.findElements(By.cssSelector(".selected li#doc"));
-        int i = 1;
-        for (WebElement e : list) {
-            e.click();
-            List<WebElement> docs = driver.findElements(By.cssSelector("li#doc"));
-            System.out.println(docs.size());
-            for (WebElement d : docs) {
-                d.click();
-                wait.until(presenceOfElementLocated(By.cssSelector("#content h1")));
-                System.out.println(i++);
+        for (int i = 1; i <= list.size(); i++) {
+            driver.findElement(By.cssSelector("#box-apps-menu li#app-:nth-of-type(" + i + ")")).click();
+            List<WebElement> sublist = driver.findElements(By.cssSelector(".docs li"));
+
+            for (int j = 1; j <= sublist.size(); j++) {
+                driver.findElement(By.cssSelector(".selected li[id^=doc]:nth-of-type(" + j + ")")).click();
+                Assertions.assertTrue(existsElement("h1"));
             }
-
         }
+    }
 
+    private boolean existsElement(String name) {
+        try {
+            driver.findElement(By.tagName(name));
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+        return true;
     }
 }
