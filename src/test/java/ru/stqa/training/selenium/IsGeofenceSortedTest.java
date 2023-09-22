b00c65cc0bac9b2f37cf11badaa3b6ba9f78c9e1
Ivan Avdeyev <97154352+Ragnazar@users.noreply.github.com>
2023-09-22 07:42:01 +0300

    Задание 9. Проверить сортировку геозон на странице геозон

---
+++ src/test/java/ru/stqa/training/selenium/IsGeofenceSortedTest.java
@@ -0,0 +1,77 @@
+package ru.stqa.training.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class IsGeofenceSortedTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+    @BeforeEach
+    public void setUp() {
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        driver.get("http://localhost/litecart/admin?app=geo_zones&doc=geo_zones");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+    }
+
+    @AfterEach
+    public void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void zonesAreSorted() {
+        List<String> zones = new ArrayList<>();
+
+        List<WebElement> list;
+        int count = driver.findElements(By.cssSelector(".dataTable tr.row")).size();
+        for (int i = 1; i < count; i++) {
+            driver.findElement(By.cssSelector("tr:nth-child(2) td:nth-child(3) a")).click();
+            list = driver.findElements(By.cssSelector("#table-zones tr:not([class='header'])"));
+            for (int j = 0; j < list.size() - 1; j++) {
+
+                zones.add(list.get(j).findElement(By.cssSelector("td:nth-child(3) select option")).getText());
+                System.out.println(list.get(j).findElement(By.cssSelector("td:nth-child(3) select option")).getAttribute("textContent"));
+            }
+            assertTrue(isSorted(zones));
+            driver.navigate().back();
+
+        }
+    }
+
+    private boolean isSorted(List<String> list) {
+        String init = "";
+
+        for (final String current : list) {
+            if (current.compareTo(init) < 0)
+                return false;
+            init = current;
+        }
+
+        return true;
+    }
+}
