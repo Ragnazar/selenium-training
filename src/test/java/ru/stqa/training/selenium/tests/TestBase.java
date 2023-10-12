1b626eddb81474f980351776230c4b211b0f0186
Ivan Avdeyev <97154352+Ragnazar@users.noreply.github.com>
2023-10-12 22:04:29 +0300

    add WebDriverManager

--- src/test/java/ru/stqa/training/selenium/tests/TestBase.java
+++ src/test/java/ru/stqa/training/selenium/tests/TestBase.java
@@ -1,12 +1,19 @@
 package ru.stqa.training.selenium.tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import ru.stqa.training.selenium.app.Application;
 
 public class TestBase {
 
     public static ThreadLocal<Application> tlApp = new ThreadLocal<>();
     public Application app;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
 
     @Before
     public void start() {
@@ -19,7 +26,10 @@ public class TestBase {
         tlApp.set(app);
 
         Runtime.getRuntime().addShutdownHook(
-                new Thread(() -> { app.quit(); app = null; }));
+                new Thread(() -> {
+                    app.quit();
+                    app = null;
+                }));
     }
 
 }
